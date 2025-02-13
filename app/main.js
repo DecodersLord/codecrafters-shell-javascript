243e556fbd223e42a4050f8984f8fa9b9e56026f
DecodersLord <PRIYANK.SEVAK1996@GMAIL.COM>
2025-02-13 15:16:24 -0500

    Initial commit

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 // Uncomment this block to pass the first stage
-// rl.question("$ ", (answer) => {
-//   rl.close();
-// });
+rl.question("$ ", (answer) => {
+    rl.close();
+});
