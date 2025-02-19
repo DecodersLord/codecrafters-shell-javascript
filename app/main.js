1b526d4aa2ded686441a822b3555330dbd3a28fb
DecodersLord <PRIYANK.SEVAK1996@GMAIL.COM>
2025-02-18 22:10:27 -0500

    file system

--- app/main.js
+++ app/main.js
@@ -1,20 +1,32 @@
-const readline = require("readline");
+const readline = require("readline/promises");
 const path = require("path");
 const fs = require("fs");
-
-const commands = ["help", "clear", "exit", "echo", "type"];
+const { execFileSync } = require("node:child_process");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-function checkCommand(command) {
-    if (commands.includes(command)) {
+function handleInvalid(answer) {
+    rl.write(`${answer}: command not found\n`);
+}
+
+function handleExit() {
+    rl.close();
+}
+
+function handleEcho(answer) {
+    rl.write(`${answer.split(" ").slice(1).join(" ")}\n`);
+}
+
+function handleType(answer) {
+    const command = answer.split(" ")[1];
+    const commands = ["exit", "echo", "type"];
+    if (commands.includes(command.toLowerCase())) {
         rl.write(`${command} is a shell builtin\n`);
     } else {
         const paths = process.env.PATH.split(":");
-
         for (const pathEnv of paths) {
             let destPath = path.join(pathEnv, command);
             if (fs.existsSync(destPath) && fs.statSync(destPath).isFile()) {
@@ -25,22 +37,46 @@ function checkCommand(command) {
         rl.write(`${command}: not found\n`);
     }
 }
-// Uncomment this block to pass the first stage
 
-async function REPLFunction() {
-    rl.question("$ ", (answer) => {
-        if (answer.startsWith("type ")) {
-            checkCommand(answer.substring("type ".length));
-        } else if (answer.startsWith("echo ")) {
-            rl.write(`${answer.substring(5)}\n`);
-        } else if (answer === "exit 0") {
-            process.exit(0);
-        } else {
-            rl.write(`${answer}: command not found\n`);
+function handleFile(answer) {
+    const fileName = answer.split(" ")[0];
+    const args = answer.split(" ").slice(1);
+    const paths = process.env.PATH.split(":");
+    for (const pathEnv of paths) {
+        let destPath = path.join(pathEnv, fileName);
+        if (fs.existsSync(destPath) && fs.statSync(destPath).isFile()) {
+            execFileSync(destPath, args, {
+                encoding: "utf-8",
+                stdio: "inherit",
+            });
         }
-
-        REPLFunction();
-    });
+    }
 }
 
-REPLFunction();
+async function question() {
+    const answer = await rl.question("$ ");
+
+    if (answer.startsWith("invalid")) {
+        handleInvalid(answer);
+        question();
+    } else {
+        switch (answer.split(" ")[0].toLowerCase()) {
+            case "exit":
+                handleExit();
+                break;
+            case "echo":
+                handleEcho(answer);
+                question();
+                break;
+            case "type":
+                handleType(answer);
+                question();
+                break;
+            default:
+                handleFile(answer);
+                question();
+        }
+    }
+}
+
+question();
